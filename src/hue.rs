6212ed573666e80faa7622d48edef9d45b665975
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-01-06 23:16:02 +1100

    Allow creating a non-default Hue through public API

--- src/hue.rs
+++ src/hue.rs
@@ -5,6 +5,19 @@ pub struct Hue {
 }
 
 impl Hue {
+    /// Creates a new `Hue` from a hue in degrees (from 0 to 360).
+    pub fn from_degrees(degrees: f32) -> Self {
+        let unnormalized_degrees = if degrees > 180.0 {
+            degrees - 360.0
+        } else {
+            degrees
+        };
+
+        Self {
+            unnormalized_degrees,
+        }
+    }
+
     /// The hue in degrees (from 0 to 360).
     pub fn degrees(self) -> f32 {
         if self.unnormalized_degrees < 0.0 {
