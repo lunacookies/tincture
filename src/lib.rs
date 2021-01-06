35fecf0fc4464d297f2560fe8bff5668f64d039a
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-01-06 23:38:51 +1100

    Flesh out crate-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,50 @@
 //! A crate for converting colors between different color spaces.
+//!
+//! Color spaces can be converted between one another using [`convert`]:
+//!
+//! ```
+//! use tincture::{LinearRgb, Oklab};
+//!
+//! let rebeccapurple = LinearRgb {
+//!     r: 0.4,
+//!     g: 0.2,
+//!     b: 0.6,
+//! };
+//!
+//! let oklab: Oklab = tincture::convert(rebeccapurple);
+//!
+//! assert_eq!(
+//!     oklab,
+//!     Oklab {
+//!         l: 0.66066486,
+//!         a: 0.079970956,
+//!         b: -0.095915854,
+//!     },
+//! );
+//! ```
+//!
+//! Variations on the core color spaces do not implement the necessary trait to use [`convert`] directly.
+//! Instead, they implement `From<ACoreColorSpace>`, allowing you to convert this variation to its corresponding core color space and call [`convert`]:
+//!
+//! ```
+//! use tincture::{Hue, LinearRgb, Oklab, Oklch, Srgb};
+//!
+//! // `Oklch` is a variation on `Oklab` (`Oklch` uses polar coordinates).
+//! let peach = Oklch {
+//!     l: 0.8,
+//!     c: 0.25,
+//!     h: Hue::from_degrees(40.0),
+//! };
+//!
+//! // This means we can create an `Oklab` using `From`.
+//! let oklab = Oklab::from(peach);
+//!
+//! // We can now convert `oklab` to any other core color space, such as `LinearRgb`.
+//! let linear_rgb: LinearRgb = tincture::convert(oklab);
+//!
+//! // `Srgb` is a variant of `LinearRgb`, so we again create one using `From`.
+//! let srgb = Srgb::from(linear_rgb);
+//! ```
 
 #![warn(missing_debug_implementations, missing_docs, rust_2018_idioms)]
 #![allow(clippy::excessive_precision)]
