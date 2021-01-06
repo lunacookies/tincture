2fb5f0c999adcae5d08eab32c71edd4152af7221
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-01-06 22:44:13 +1100

    Use imperative mood for documentation

--- src/lib.rs
+++ src/lib.rs
@@ -15,14 +15,14 @@ pub use xyz::Xyz;
 
 /// A trait that describes what behavior a color must have to interoperate with the rest of the system.
 pub trait Color {
-    /// Converts a color in the XYZ color space to the color space that `Self` represents.
+    /// Convert a color in the XYZ color space to the color space that `Self` represents.
     fn from_xyz(xyz: Xyz) -> Self;
 
-    /// Converts the color of `Self` to the XYZ color space.
+    /// Convert the color of `Self` to the XYZ color space.
     fn to_xyz(self) -> Xyz;
 }
 
-/// Converts a color from one color space to another.
+/// Convert a color from one color space to another.
 pub fn convert<In: Color, Out: Color>(color: In) -> Out {
     let xyz = color.to_xyz();
     Out::from_xyz(xyz)
