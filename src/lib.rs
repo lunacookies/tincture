7a2f9414a127e1e1ea2ee37bc595fd9e9076e8b3
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-01-06 18:05:14 +1100

    Add machinery to convert between different colour spaces

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,18 @@ mod xyz;
 pub use oklab::Oklab;
 pub use rgb::Rgb;
 pub use xyz::Xyz;
+
+/// A trait that describes what behavior a color must have to interoperate with the rest of the system.
+pub trait Color {
+    /// Converts a color in the XYZ color space to the color space that `Self` represents.
+    fn from_xyz(xyz: Xyz) -> Self;
+
+    /// Converts the color of `Self` to the XYZ color space.
+    fn to_xyz(self) -> Xyz;
+}
+
+/// Converts a color from one color space to another.
+pub fn convert<In: Color, Out: Color>(color: In) -> Out {
+    let xyz = color.to_xyz();
+    Out::from_xyz(xyz)
+}
