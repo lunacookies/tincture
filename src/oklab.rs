b1389be1d07757f69865ddf2bdcc2bbf7a43768a
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-01-06 17:58:38 +1100

    Add line breaks between sentences

--- src/oklab.rs
+++ src/oklab.rs
@@ -3,10 +3,13 @@
 /// [Oklab]: https://bottosson.github.io/posts/oklab/
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Default)]
 pub struct Oklab {
-    /// Lightness. 0 is complete black, 1 is the brightest white.
+    /// Lightness.
+    /// 0 is complete black, 1 is the brightest white.
     pub l: f32,
-    /// Green vs red. -1 is green, 1 is red.
+    /// Green vs red.
+    /// -1 is green, 1 is red.
     pub a: f32,
-    /// Blue vs yellow. -1 is blue, 1 is yellow.
+    /// Blue vs yellow.
+    /// -1 is blue, 1 is yellow.
     pub b: f32,
 }
